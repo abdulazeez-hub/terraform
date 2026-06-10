08bbe1840952266e3e12b8a8b8539aaaba8a9023
abdulazeez-hub <abdulazeez@metasysglobal.com>
2026-06-10 04:04:47 +0100

    fixed module issue

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -32,6 +32,7 @@ resource "aws_instance" "this" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name = var.key_name
 
   tags = {
     Name = var.instance_name
